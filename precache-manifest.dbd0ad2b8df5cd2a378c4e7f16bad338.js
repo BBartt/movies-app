799a725d8f5b9a3fdf12f5e605a363ccc196400d
esbbartt <bartosz.polakowski@escola.pl>
2021-02-03 21:57:11 +0100

    Updates

---
+++ precache-manifest.dbd0ad2b8df5cd2a378c4e7f16bad338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5238d72444618463bba2",
+    "url": "/movies-app/static/js/main.d3aea31b.chunk.js"
+  },
+  {
+    "revision": "f58da00be1d28636e05a",
+    "url": "/movies-app/static/js/runtime~main.41022d0a.js"
+  },
+  {
+    "revision": "69cd7a25f531d1a3afcc",
+    "url": "/movies-app/static/js/2.a7848438.chunk.js"
+  },
+  {
+    "revision": "b868becf670a4d8a7c8d27bf0e17360a",
+    "url": "/movies-app/static/media/beige-tiles.b868becf.png"
+  },
+  {
+    "revision": "8d30720364106120d7903f3be7c4ca69",
+    "url": "/movies-app/index.html"
+  }
+];
